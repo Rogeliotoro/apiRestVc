eae708492830b88c281c83608ca059bbf709d24b
Rogeliotoro <rogeltoro@gmail.com>
2022-03-20 23:18:09 +0100

    prueba5 apirestvc

--- src/movies/controllers.js
+++ src/movies/controllers.js
@@ -23,13 +23,4 @@ const getMovies = (req, res)=>{
      .catch((error)=> res.json({mesanje: error}));
  };
 
- const getMoviesGe = (req , res) => {
-    const {genero} = req.body;
-    userSchema
-    .findOne(genero)
-    .then((data) => res.json(data))
-    .catch((error)=> res.json({mesanje: error}));
-};
-
-
- module.exports = {getMovies,postMovies, getMoviesId, getMoviesGe}
+ module.exports = {getMovies,postMovies, getMoviesId}
--- src/movies/routes.js
+++ src/movies/routes.js
@@ -1,11 +1,10 @@
 const express = require ('express');
 const router = express.Router();
-const {getMovies, postMovies, getMoviesId, getMoviesGe} = require('./controllers.js');
+const {getMovies, postMovies, getMoviesId} = require('./controllers.js');
 
 router.get("/movies", getMovies);
 router.post("/movies", postMovies);
 router.get('/movies/:id', getMoviesId);
-router.get('/movies', getMoviesGe);
 
 module.exports = router
 
